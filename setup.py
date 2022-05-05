106a6137925c72a573f765df29ee406156a50e87
Robin Thibaut <robin.thibaut@ugent.be>
2022-05-05 20:07:53 +0200

    Added stuff

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+my_pckg = find_packages(include=["wbi"])
+with open("README.rst") as f:
+    LONG_DESCRIPTION = f.read()
+setup(
+    name="wbi",
+    version="0.1.0",
+    packages=my_pckg,
+    include_package_data=True,
+    url="",
+    license="BSD-3",
+    author="",
+    author_email="",
+    description="",
+    long_description=LONG_DESCRIPTION,
+    install_requires=["numpy", "scipy", "cython", "simpeg", "PyWavelets"],
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    python_requires=">=3.7",
+)
